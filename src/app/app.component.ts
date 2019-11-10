7a79c438ee419239f603830fbb4db6c120845dae
Anthony <arosado-26@hotmail.com>
2019-11-09 20:03:51 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,77 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { ProductosService } from './productos.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'lab10-mean';
+
+  lista = null;
+  prod = {
+    _id: null,
+    descripcion: null,
+    precio: null
+  };
+
+  constructor(private productosServicio: ProductosService) {}
+
+  ngOnInit() {
+    this.recuperarTodos();
+  }
+
+  recuperarTodos() {
+    this.productosServicio.listar().subscribe(result => {
+      this.lista = result;
+    });
+  }
+
+  nuevo() {
+    this.productosServicio.nuevo(this.prod).subscribe(result => {
+      if (result === 'ok') {
+        this.limpiar();
+        this.recuperarTodos();
+      }
+    });
+  }
+
+  eliminar(codigo) {
+    if (!confirm('Esta seguro que desea eliminar este registro?'))
+      return;
+    this.productosServicio.eliminar(codigo).subscribe(result => {
+      if (result === 'ok') {
+        this.recuperarTodos();
+      }
+    });
+  }
+
+  actualizar() {
+    this.productosServicio.actualizar(this.prod).subscribe(result => {
+      // @ts-ignore
+      if (result.nModified === '1') {
+        this.limpiar();
+        this.recuperarTodos();
+      }
+    });
+  }
+
+  mostrar(codigo) {
+    this.productosServicio.mostrar(codigo).subscribe(result => {
+      // @ts-ignore
+      this.prod = result;
+    });
+  }
+
+  hayRegistros() {
+    return true;
+  }
+  limpiar() {
+    this.prod = {
+      _id: null,
+      descripcion: null,
+      precio: null
+    };
+  }
 }
